b97cfd2e9ce2e36562f9fb9a0a8400055b309ca2
João da Cruz <102554786+JohnDaCruz@users.noreply.github.com>
2023-05-22 19:44:06 -0300

    controller

--- src/book/book.router.ts
+++ src/book/book.router.ts
@@ -3,6 +3,9 @@ import { Request, Response } from "express"
 import { body, validationResult } from "express-validator"
 
 import * as BookService from "./book.service"
+import { isDate } from "util/types";
+import { request } from "http";
+import { db } from "../utils/db.server";
 
 export const bookRouter = express.Router();
 
@@ -18,7 +21,7 @@ bookRouter.get("/", async (req: Request, res: Response) => {
 bookRouter.get("/:id", async (req: Request, res: Response) => {
     const id: number = parseInt(req.params.id, 10);
     try {
-        const book = await BookService.getBook(id);
+        const book = await BookService.getBooks(id);
         if (book) {
             return res.status(200).json(book);
         }
@@ -27,3 +30,55 @@ bookRouter.get("/:id", async (req: Request, res: Response) => {
         return res.status(400).json(error.message);
     }
 });
+
+bookRouter.post("/",
+    body("title").isString(),
+    body("authoriD").isInt(),
+    body("publicado").isDate().toDate(),
+    body("isFiction").isBoolean(),
+    async (req: Request, res: Response) => {
+        const erros = validationResult(req);
+        if (!erros.isEmpty()) {
+            return res.status(400).json({ erros: erros.array() });
+        }
+        try {
+            const book = req.body
+            const novoLivro = await BookService.novoLivro(book)
+            return res.status(201).json(novoLivro)
+        } catch (error: any) {
+            return res.status(500).json(error.message);
+        }
+    }
+)
+
+bookRouter.put("/:id",
+    body("title").isString(),
+    body("authoriD").isInt(),
+    body("publicado").isDate().toDate(),
+    body("isFiction").isBoolean(),
+    async (req: Request, res: Response) => {
+        const erros = validationResult(req);
+        if (!erros.isEmpty()) {
+            return res.status(400).json({ erros: erros.array() });
+        }
+        const id: number = parseInt(req.params.id, 10);
+        try {
+            const book = req.body
+            const atualizaBook = await BookService.atualizarBook(book, id)
+            return res.status(201).json(atualizaBook)
+        } catch (error: any) {
+            return res.status(500).json(error.message);
+        }
+    }
+)
+
+bookRouter.delete("/:id", async (req: Request, res: Response) => {
+    const id: number = parseInt(req.params.id, 10)
+    try {
+        await BookService.deleteBook(id)
+        return res.status(200).json("Livro deletado com sucesso!");
+    } catch (error: any) {
+        res.status(500).json(error.message);
+    }
+})
+
--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -9,6 +9,13 @@ type BookRead = {
     author: Author
     //authoriD: number;
 };
+type BookWrite = {
+    id: number;
+    title: string;
+    publicado: Date;
+    isFiction: boolean;
+    authoriD: number
+}
 
 export const listBooks = async (): Promise<BookRead[]> => {
     return db.book.findMany({
@@ -30,10 +37,85 @@ export const listBooks = async (): Promise<BookRead[]> => {
     })
 }
 
-export const getBook = async (id: number): Promise<BookRead | null> => {
+export const getBooks = async (id: number): Promise<BookRead | null> => {
     return db.book.findUnique({
         where: {
-            id: id,
+            id,
+        },
+        select: {
+            id: true,
+            title: true,
+            publicado: true,
+            isFiction: true,
+            author: {
+                select: {
+                    id: true,
+                    nome: true,
+                    sobrenome: true,
+                }
+            }
+        }
+    })
+}
+
+export const novoLivro = async (book: BookWrite): Promise<BookRead> => {
+    const { title, authoriD, publicado, isFiction } = book
+    const parsedDate: Date = new Date(publicado);
+    return db.book.create({
+        data: {
+            title,
+            authoriD,
+            isFiction,
+            publicado: parsedDate,
+        },
+        select: {
+            id: true,
+            title: true,
+            publicado: true,
+            isFiction: true,
+            author: {
+                select: {
+                    id: true,
+                    nome: true,
+                    sobrenome: true,
+                }
+            }
+        }
+    })
+}
+
+export const atualizarBook = async (book: BookWrite, id: number): Promise<BookRead> => {
+    const { title, isFiction, publicado, authoriD } = book
+    return db.book.update({
+        where: {
+            id
+        },
+        data: {
+            title,
+            isFiction,
+            publicado,
+            authoriD
+        },
+        select: {
+            id: true,
+            title: true,
+            publicado: true,
+            isFiction: true,
+            author: {
+                select: {
+                    id: true,
+                    nome: true,
+                    sobrenome: true,
+                }
+            }
+        }
+    })
+}
+
+export const deleteBook = async (id: number): Promise<void> => {
+    await db.book.delete({
+        where: {
+            id,
         }
     })
 }
